73714a1d95cdb65430caef99d0ecab49705ebc93
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-08-01 18:34:45 +0700

    INTEGRATION shuttle.rs SERVER

--- flake.nix
+++ flake.nix
@@ -6,13 +6,10 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     devenv.url = "github:cachix/devenv";
+    crane.url = "github:ipetkov/crane";
 
     fenix = {
       url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane = {
-      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
